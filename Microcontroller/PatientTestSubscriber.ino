c5ea8e99f1553f1c425854aa1b0200e57eac3618
Sarrankan <sarran_01@hotmail.ca>
2021-07-08 14:33:49 -0400

    Added bpm to test results.
    Added instructions via BLE for heart rate monitoring
    Added delay to instructions
    Added testBegin, to wait for valid patient biometric before beginning a test on the subscriber microcontroller

--- Microcontroller/PatientTestSubscriber.ino
+++ Microcontroller/PatientTestSubscriber.ino
@@ -14,16 +14,22 @@ const int LEDBLUE = D13; // D4 - gpio2
 const int LEDGREEN = D10;
 SoftwareSerial HM19;
 
-bool reactionTimeCompleted = false;
-bool tempHumidityCompleted = false;
+///TEST VARIABLES//
 bool instructed = false;
 bool done = false;
-int distance = 5000;
+bool testBegin = false;
+bool reactionTimeCompleted = false;
+bool tempHumidityCompleted = false;
+bool heartRateCompleted = false;
+
+///Globals
 float temperature;
 float humidity;
-int monitorId = 0;
+int BPM;
 String patientId;
+int monitorId = 0;
 uint addr = 0;
+int distance = 5000;
 
 struct{
   uint val = 0;
@@ -68,6 +74,7 @@ void setup(){
   client.subscribe("v1/DHTsensor/roomtempandhumidity");
   client.subscribe("v1/Adafruit/fingerprint");
   client.subscribe("v1/test/testData");
+  client.subscribe("v1/pulseSensor/Heart");
   digitalWrite(LEDGREEN,HIGH); //Ready.
 }
 
@@ -84,17 +91,22 @@ void ReceivedMessage(char*topic, byte*payload, unsigned int length) {
   if(doc["id"] == "arduino_Distance"){
     distance = doc["distance"];
     monitorId = doc["monitorId"];
+  }else if(doc["id"] == "arduino_PulseSensor"){
+    BPM = doc["bpm"];
   }else if(doc["id"] == "arduino_DHT11"){
     temperature = doc["temperature"];
     humidity = doc["humidity"];
   }else if(doc["id"] > 0){
     if(doc["id"] == 1){
       patientId = "Patient_1";
+      testBegin = true;
     }else if(doc["id"] == 2){
       patientId = "Patient_2";
+      testBegin = true;
     }
   }
 }
+
 
 DynamicJsonDocument reactionTimeTest(){
   DynamicJsonDocument response(1024);
@@ -110,7 +122,7 @@ DynamicJsonDocument reactionTimeTest(){
     }
     unsigned long reactionTime = millis() - currentTime;
     response["reactionTime"] = reactionTime;
-    HM19.println("Reaction Time Recorded :) Test 1/5 completed");
+    HM19.println("Reaction Time Recorded :) Test 1/4 completed");
     reactionTimeCompleted = true;
     instructed = false;
     return response;   
@@ -130,40 +142,71 @@ DynamicJsonDocument temperatureHumidityTest(){
   tempHumidityCompleted = true;
   instructed = false;
   digitalWrite(LEDBLUE, HIGH);
-  HM19.println("Temperature and Humidity Recorded :) Test 2/5 completed");
+  HM19.println("Temperature and Humidity Recorded :) Test 2/4 completed");
+  return response;
+}
+
+DynamicJsonDocument HeartRateTest(){
+  DynamicJsonDocument response(1024);
+  char attributes[100];
+  delay(10000);
+  client.loop();
+  response["BPM"] = BPM;
+  instructed = false;
+  digitalWrite(LEDBLUE, HIGH);
+  HM19.println("Heart Rate Recorded :) Test 3/4 completed");
+  heartRateCompleted = true;
   return response;
 }
 
 void loop() {
   DynamicJsonDocument reactionTime(1024);
   DynamicJsonDocument temperatureHumidity(1024);
-  if(!reactionTimeCompleted){
-    if(!instructed){
-      HM19.println("Please remain seated.");
-      HM19.println("Place your right hand infront of the distance sensor until the BLUE LED has turned on");
-      HM19.println("Remove your hand once the BLUE LED has turned off");
-      instructed = true;
+  DynamicJsonDocument heartRate(1024);
+  if(testBegin == true){
+    if(!reactionTimeCompleted){
+      if(!instructed){
+        HM19.println("Please remain seated.");
+        HM19.println("Place your right hand infront of the distance sensor until the BLUE LED has turned on");
+        HM19.println("Remove your hand once the BLUE LED has turned off");
+        instructed = true;
+      }
+     reactionTime = reactionTimeTest();
     }
-   reactionTime = reactionTimeTest();
-  }
-  if(!tempHumidityCompleted && reactionTimeCompleted){
-    if(!instructed){
-      HM19.println("Please place your right index finger on the temperature sensor");
-      HM19.println("Please wait until the BLUE LED has turned on");
-      instructed = true;
+    if(!tempHumidityCompleted && reactionTimeCompleted){
+      if(!instructed){
+        HM19.println("Please place your right index finger on the temperature sensor");
+        delay(1000);
+        HM19.println("Please wait until the BLUE LED has turned on");
+        instructed = true;
+      }
+      temperatureHumidity = temperatureHumidityTest();
+      delay(2000);
+      digitalWrite(LEDBLUE, LOW);
     }
-    temperatureHumidity = temperatureHumidityTest();
-    delay(2000);
-    digitalWrite(LEDBLUE, LOW);
-  }
-  if(tempHumidityCompleted && reactionTimeCompleted && !done){
-    sendTestData(reactionTime,temperatureHumidity);
-    done = true;
+    if(!heartRateCompleted && tempHumidityCompleted && reactionTimeCompleted){
+      if(!instructed){
+        HM19.println("Please place a finger on the pulse sensor to measure BPM");
+        delay(1000);
+        HM19.println("Use the enclosure to ensure obscurity between sensor and finger");
+        delay(1000);
+        HM19.println("Pleae wait until the BLUE LED has turned on");
+        instructed = true;
+      }
+      heartRate = HeartRateTest();
+      delay(2000);
+      digitalWrite(LEDBLUE, LOW);
+    } 
+    if(tempHumidityCompleted && reactionTimeCompleted && heartRateCompleted && !done){
+      sendTestData(reactionTime,temperatureHumidity,heartRate);
+      done = true;
+      testBegin = false;
+    }
   }
   client.loop();
 }
  
-void sendTestData(DynamicJsonDocument reactionTimeTest, DynamicJsonDocument temperatureHumidityTest){
+void sendTestData(DynamicJsonDocument reactionTimeTest, DynamicJsonDocument temperatureHumidityTest, DynamicJsonDocument heartRate){
   DynamicJsonDocument testData(1024);
   char attributes[1000];
   int testNumber = data.val;
@@ -174,6 +217,7 @@ void sendTestData(DynamicJsonDocument reactionTimeTest, DynamicJsonDocument temp
   testData["reactionTime"] = reactionTimeTest["reactionTime"];
   testData["temperature"] = temperatureHumidityTest["temperature"];
   testData["humidity"] = temperatureHumidityTest["humidity"];
+  testData["bpm"] = heartRate["BPM"];
   serializeJson(testData, attributes);
   client.publish("v1/test/testData",attributes);
   data.val += 1;
